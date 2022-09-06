15481d57f3117644cfa0d88f0d42fc59e204b67b
yishibakaien <646932161@qq.com>
2022-09-06 11:39:33 +0800

    chore: 实体 -> 标签

--- example/app.tsx
+++ example/app.tsx
@@ -1,22 +1,21 @@
-import React from "react"
-import { render } from "react-dom"
-import { LabelImgProvider } from "./label-img-provider"
-import Control from "./control"
-import { StoreProvider } from "./store-provider"
-import Listener from "./listener"
-import { CreateInstance } from "./instance"
-import { Row } from "antd"
-import "./app.less"
+import React from 'react'
+import { render } from 'react-dom'
+import { LabelImgProvider } from './label-img-provider'
+import Control from './control'
+import { StoreProvider } from './store-provider'
+import Listener from './listener'
+import { CreateInstance } from './instance'
+import { Row } from 'antd'
+import './app.less'
 
 const Main = () => {
-
   return (
     <StoreProvider>
       <LabelImgProvider>
-        <div className="pw">
-          <Row justify="center">
-            <CreateInstance />
+        <div className='pw'>
+          <Row justify='center'>
             <Control />
+            <CreateInstance />
             <Listener />
           </Row>
         </div>
@@ -25,4 +24,4 @@ const Main = () => {
   )
 }
 
-render(<Main />, document.getElementById("app"))
+render(<Main />, document.getElementById('app'))
--- example/control.tsx
+++ example/control.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useRef } from "react"
-import { Button, Tag, Switch, Modal, Image, Row, Col, Divider, Tabs } from "antd"
-import SourceModal from "./source-modal"
+import React, { useState, useRef } from 'react'
+import { Button, Tag, Switch, Modal, Image, Row, Col, Divider, Tabs } from 'antd'
+import SourceModal from './source-modal'
 
-import { useLabelImg } from "./label-img-provider"
-import { dataURIToBlob } from "../src/utils"
-import { useStore } from "./store-provider"
-import { Shape } from "../src/Shape"
-import EntityModal from "./entity-modal"
-import "./control.less"
+import { useLabelImg } from './label-img-provider'
+import { dataURIToBlob } from '../src/utils'
+import { useStore } from './store-provider'
+import { Shape } from '../src/Shape'
+import EntityModal from './entity-modal'
+import './control.less'
 
 const { TabPane } = Tabs
 
@@ -19,50 +19,57 @@ const ShapeItem = ({ shape, render }: { shape: Shape; render: Function }) => {
   const isDisabled = shape.isDisabled()
 
   return (
-    <div className="shape-item">
-      <div>
-        {`${registerID}-${id}`}
-      </div>
-      <div className="shape-ctrl">
-        <Button size="small" onClick={() => {
-          if(isHidden){
-            shape.show()
-          }else{
-            shape.hidden()
-          }
-          render()
-        }}>
+    <div className='shape-item'>
+      <div>{`${registerID}-${id}`}</div>
+      <div className='shape-ctrl'>
+        <Button
+          size='small'
+          onClick={() => {
+            if (isHidden) {
+              shape.show()
+            } else {
+              shape.hidden()
+            }
+            render()
+          }}
+        >
           {isHidden ? '显示' : '隐藏'}
         </Button>
-        <Button size="small" onClick={() => {
-          if(isDisabled){
-            shape.normal()
-          }else{
-            shape.disabled()
-          }
-          render()
-
-        }}>
+        <Button
+          size='small'
+          onClick={() => {
+            if (isDisabled) {
+              shape.normal()
+            } else {
+              shape.disabled()
+            }
+            render()
+          }}
+        >
           {isDisabled ? '正常' : '禁用'}
         </Button>
-        <Button size="small" danger onClick={() => {
-          lb?.remove(shape)
-          render()
-
-        }}>
+        <Button
+          size='small'
+          danger
+          onClick={() => {
+            lb?.remove(shape)
+            render()
+          }}
+        >
           删除
         </Button>
-        <Button size="small" onClick={() => {
-          shape.tagShow()
-          render()
-        }}>
+        <Button
+          size='small'
+          onClick={() => {
+            shape.tagShow()
+            render()
+          }}
+        >
           标签
         </Button>
       </div>
       <div>
-        <Tag color="blue">
-          {type}
-        </Tag>
+        <Tag color='blue'>{type}</Tag>
       </div>
     </div>
   )
@@ -73,19 +80,19 @@ const Control = () => {
   const [{ list, labelTypes }, setStore] = useStore()
   const [continuity, setContinuity] = useState(false)
   const [isCreate, setIsCreate] = useState(false)
-  const [base64, setBase64] = useState("")
+  const [base64, setBase64] = useState('')
 
   const render = () => {
     const list = lb?.getShapeList()
-    if(!list) return
+    if (!list) return
     setStore({
       list
     })
     lb?.render()
   }
-  
+
   const cb = (v: any) => {
-    if(!lb) return
+    if (!lb) return
     const { id, ...options } = v
     lb.register(id, options)
     const labelTypes = lb.getLabels()
@@ -96,74 +103,97 @@ const Control = () => {
   }
 
   return (
-    <div className="control">
+    <div className='control'>
       <Divider></Divider>
-      <Row justify="space-between" align="middle">
-        <Col className="gutter-row" span={8}>
-          <a href="https://github.com/hold-baby/label-img">
-            go to github
-          </a>
-        </Col>
+      <Row justify='space-between' align='middle'>
         <Col span={8}>
-          <Button onClick={() => {
-            setIsCreate(true)
-          }}>
-            新建实体类型
+          <Button
+            onClick={() => {
+              setIsCreate(true)
+            }}
+          >
+            新建标签种类
           </Button>
         </Col>
-        <EntityModal visible={isCreate} onCancel={() => {
-          setIsCreate(false)
-        }} cb={cb} />
+        <EntityModal
+          visible={isCreate}
+          onCancel={() => {
+            setIsCreate(false)
+          }}
+          cb={cb}
+        />
       </Row>
-      <Divider orientation="left">实体类型列表</Divider>
-      <Row justify="start">
+      <Divider orientation='left'>可选标签种类</Divider>
+      <Row justify='start'>
         {labelTypes.map(({ key, name }) => {
           return (
-            <Col style={{
-              marginRight: 8
-            }}>
-              <Button key={key} size="small" onClick={() => {
-                lb?.label(key)
-              }}>
+            <Col
+              style={{
+                marginRight: 8
+              }}
+            >
+              <Button
+                key={key}
+                size='small'
+                onClick={() => {
+                  lb?.label(key)
+                }}
+              >
                 {name}
               </Button>
             </Col>
           )
         })}
       </Row>
-      <Divider orientation="left">控制</Divider>
-      <div className="continuity">
-        <Switch onChange={(continuity) => {
-          setContinuity(continuity)
-          lb?.setContinuity(continuity)
-        }} />
-        {continuity ? '连续标注': '单次标注'}
+      <Divider orientation='left'>控制</Divider>
+      <div className='continuity'>
+        开启连续标注：
+        <Switch
+          onChange={(continuity) => {
+            setContinuity(continuity)
+            lb?.setContinuity(continuity)
+          }}
+        />
       </div>
-      <Row justify="start" align="middle">
-          <Button size="small" onClick={() => {
+      <Row justify='start' align='middle'>
+        <Button
+          size='small'
+          onClick={() => {
             lb?.setTagShow(!lb.isTagShow())
-          }}>
-            显示/隐藏标签
-          </Button>
-          <Button size="small" onClick={() => {
+          }}
+        >
+          显示/隐藏标签
+        </Button>
+        <Button
+          size='small'
+          onClick={() => {
             lb?.getShapeList().forEach((shape) => {
               shape.isHidden() ? shape.show() : shape.hidden()
             })
             lb?.render()
-          }}>
-            显示/隐藏图形
-          </Button>
-          <Button size="small" onClick={() => {
+          }}
+        >
+          显示/隐藏图形
+        </Button>
+        <Button
+          size='small'
+          onClick={() => {
             lb?.resize()
-          }}>
-            重置大小
-          </Button>
-          <Button size="small" onClick={() => {
+          }}
+        >
+          重置大小
+        </Button>
+        <Button
+          size='small'
+          onClick={() => {
             lb?.setGuideLine()
-          }}>
-            辅助线
-          </Button>
-          <Button size="small" onClick={() => {
+          }}
+        >
+          辅助线
+        </Button>
+        <Button
+          size='small'
+          onClick={() => {
             const list = lb?.getShapeList().map(({ id, tagContent, positions }) => {
               return {
                 id,
@@ -171,50 +201,58 @@ const Control = () => {
                 positions
               }
             })
-            console.log(list);
+            console.log(list)
             alert(JSON.stringify(list))
-          }}>
-            获取数据
-          </Button>
-          <Button size="small" onClick={() => {
+          }}
+        >
+          获取数据
+        </Button>
+        <Button
+          size='small'
+          onClick={() => {
             const base64 = lb?.toDataURL()
-            if(!base64) return
+            if (!base64) return
             setBase64(base64)
-          }}>
-            导出图片
-          </Button>
-          <Modal visible={!!base64} onCancel={() => {
-            setBase64("")
-          }} cancelText="关闭" onOk={() => {
+          }}
+        >
+          导出图片
+        </Button>
+        <Modal
+          visible={!!base64}
+          onCancel={() => {
+            setBase64('')
+          }}
+          cancelText='关闭'
+          onOk={() => {
             const blob = dataURIToBlob(base64)
-            const a = document.createElement("a")
+            const a = document.createElement('a')
             a.download = 'labelImage.jpg'
             a.href = URL.createObjectURL(blob)
             a.click()
-          }} okText="下载">
-            <Image src={base64} preview={false} />
-          </Modal>
+          }}
+          okText='下载'
+        >
+          <Image src={base64} preview={false} />
+        </Modal>
       </Row>
-      <Divider orientation="left">实体列表</Divider>
-      <Tabs type="card">
-        <TabPane tab="全部" key="all">
-          <div className="shape-list">
+      <Divider orientation='left'>已打标签</Divider>
+      <Tabs type='card'>
+        <TabPane tab='全部' key='all'>
+          <div className='shape-list'>
             {list.map((shape) => {
-              return (
-                <ShapeItem shape={shape} render={render} key={shape.id} />
-              )
+              return <ShapeItem shape={shape} render={render} key={shape.id} />
             })}
           </div>
         </TabPane>
         {labelTypes.map(({ key, name }) => {
           return (
             <TabPane tab={name} key={key}>
-              <div className="shape-list">
-                {list.filter(({ registerID }) => registerID === key ).map((shape) => {
-                  return (
-                    <ShapeItem shape={shape} render={render} key={shape.id} />
-                  )
-                })}
+              <div className='shape-list'>
+                {list
+                  .filter(({ registerID }) => registerID === key)
+                  .map((shape) => {
+                    return <ShapeItem shape={shape} render={render} key={shape.id} />
+                  })}
               </div>
             </TabPane>
           )
